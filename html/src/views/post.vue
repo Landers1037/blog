1106dbaa7d5777b8e8ef4bf09979803d451d8fc4
landers <liaorenj@gmail.com>
2021-07-23 00:12:00 +0800

    修复特殊字符的转义问题

--- html/src/views/post.vue
+++ html/src/views/post.vue
@@ -379,9 +379,12 @@
                 let body = document.getElementById("markdown-body");
                 let heads = body.querySelectorAll("h1, h2, h3");
                 for (let h of heads) {
+                    console.log(h);
                     let svg_icon= '<svg className="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16"\n                             aria-hidden="true">\n                            <path fill-rule="evenodd"\n                                  d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>\n</svg>'
+                    // 为避免id转义的问题使用text获取
+                    let text_inner = h.innerText;
                     let text_id = h.id;
-                    h.innerHTML = '<a class="head-link" href="#' + text_id + '">' + svg_icon + '</a>' + text_id;
+                    h.innerHTML = '<a class="head-link" href="#' + text_id + '">' + svg_icon + '</a>' + text_inner;
                 }
             },
             // 获取全部评论 并进行渲染
